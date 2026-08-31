3829a01cd8ad317863b9c92f0b3a6004856d5f66
ochoyomoses-rgb <ochoyomoses@gmail.com>
2026-08-31 20:58:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,23 +8,23 @@ def home():
     return {"message": "Flask API is running"}
 
 
-    @app.get("/hello")
-    def hello():
-        return {"message": "Hello from the GET endpoint!"}
+@app.get("/hello")
+def hello():
+    return {"message": "Hello from the GET endpoint!"}
 
 
-        @app.post("/greet")
-        def greet():
-            data = request.get_json(silent=True) or {}
-                username = data.get("username")
+@app.post("/greet")
+def greet():
+    data = request.get_json(silent=True) or {}
+    username = data.get("username")
 
-                    if not username:
-                            return {"error": "username is required"}, 400
+    if not username:
+        return {"error": "username is required"}, 400
 
-                                return {
-                                        "message": f"Hello, {username}! Welcome to my Flask API."
-                                            }
+    return {
+        "message": f"Hello, {username}! Welcome to my Flask API."
+    }
 
 
-                                            if __name__ == "__main__":
-                                                app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
